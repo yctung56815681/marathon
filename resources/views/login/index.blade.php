630a671a643ff5e83b3edd525049c416ab7de2b0
Mikl <asfx159753@gmail.com>
2019-09-25 23:08:37 +0800

    cover

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,79 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>管理者系統</title>
 
-    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
     <style>
-    .login{
-        
-        text-align: center;
-        margin:100px auto;
-        width: 500px;
-        height: 500px;
-        position: relative;
-        background-color: violet;
-        color:#fff;font-family:'微軟正黑體'; text-align:center;font-size:16px;
-        background:rgba(0,0,0,0.6);margin-bottom:10px;
-    } 
-    
-    .back{
-        background-image: url("img/1008435.jpg");
-        background-size: cover;
-       	
-    }
-    img{
-        width: 20%;
-        height: 20%;
-    }
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
 
- 
-    
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
 
-    
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-align: center;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #f5f5f5;
+        }
+
+        .form-signin {
+            width: 100%;
+            max-width: 330px;
+            padding: 15px;
+            margin: auto;
+        }
+
+        .form-signin .checkbox {
+            font-weight: 400;
+        }
+
+        .form-signin .form-control {
+            position: relative;
+            box-sizing: border-box;
+            height: auto;
+            padding: 10px;
+            font-size: 16px;
+        }
+
+        .form-signin .form-control:focus {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
     </style>
-
-
 </head>
-<body class="back">
-    <form action="http://localhost:8000/">
-        <div class="form-control dark login">
-            <img src="img/iiilogo.png" alt="">
-            <br>
-            <br>
-            <br>
-            <h2>Sign in</h2>
-            <div >
-                    <input class="form-control" placeholder="Account" id="memberAccount" type="text">
-            </div>
-            
-            <br>
-            <br>
-            <div>
-                    <input class="form-control" placeholder="Password" id="memberKeyword" type="text">
-            </div >
 
-            <br>
-            <br>
-            <div >
-                    <input class="btn-primary form-control" type="submit" value="login">
+<body>
+    <body class="text-center">
+        <form class="form-signin" method="post" action="/login">
+            @csrf
+            <img class="mb-4" src="img/iiilogo.png" alt="" width="72" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">管理者系統</h1>
+            <label for="inputEmail" class="sr-only">帳號</label>
+            {{-- <input type="email" id="inputEmail" class="form-control" placeholder="帳號" required autofocus> --}}
+            <input type="text" id="inputEmail" class="form-control" name="username" placeholder="帳號" required autofocus>
+            <label for="inputPassword" class="sr-only">密碼</label>
+            <input type="password" id="inputPassword" class="form-control" name="password" placeholder="密碼" required>
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me">記住我
+                </label>
             </div>
-            
-           
-
-        </div>
-    </form>
-   
-    
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-{{-- <body>
-    <h1>/resources/views/login/index.blade.php</h1><hr>
-    {{$view}}<hr>
-    <a href="/">Home</a> --}}
+            <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+        </form>
+    </body>
 </body>
+
 </html>
