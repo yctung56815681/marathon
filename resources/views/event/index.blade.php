7670aa66879515a5487d4a4006e90b05c9178c17
yctung56815681 <yctung56815681@gmail.com>
2019-09-25 13:27:13 +0800

    merge 9 event main pages to 1 event main page using laravel blade languafe

---
+++ resources/views/event/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    {{-- <h1>/resources/views/event/changhua202002.blade.php</h1><hr> --}}
+    {{-- <img src="{{ URL::asset('img/changhua202002.jpg') }}" id="img"/><hr> --}}
+    {{-- <a href="/login/changhua/2020/02">Sign Up</a><hr> --}}
+
+    <h1>/resources/views/event/{{$city}}{{$year}}{{$month}}.blade.php</h1><hr>
+    <img src="{{ URL::asset("img/{$city}{$year}{$month}.jpg") }}" id="img"/><hr>
+    <a href="/login/{{ $city }}/{{ $year }}/{{$month}}">Sign Up</a><hr>
+
+    <a href="/">Home</a>
+</body>
+</html>
