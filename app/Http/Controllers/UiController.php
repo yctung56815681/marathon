a8a413bcb82da95b1cb8be73ddb26ad384364018
yctung56815681 <yctung56815681@gmail.com>
2019-09-26 00:09:51 +0800

    rename files

--- app/Http/Controllers/UiController.php
+++ app/Http/Controllers/UiController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UIController extends Controller
+class UiController extends Controller
 {
     public function index()
     {
-        return view("UI.UI");
+        return view("ui.ui");
     }
 }
