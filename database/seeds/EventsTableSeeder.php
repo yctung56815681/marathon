4d1cecd218e5f025a42094699eed41ee28b10f36
WSP <nickgogogonickgogogo@hotmail.com>
2019-10-17 22:40:44 +0800

    10810172240

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -15,17 +15,10 @@ class EventsTableSeeder extends Seeder
 
             DB::table('events')->insert([
                 'cityId' => '2',
-<<<<<<< HEAD
-                'eventSignupStartTime' => '2019-07-24 09:00:00',
-                'eventSignupEndTime' => '2019-8-24 09:00:00',
-                'eventRunStartTime' => '2019-11-24 07:00:00',
-                'eventRunEndTime' => '2019-11-24 17:00:00',
-=======
                 'eventSignupStartTime' => '2019-08-24 09:00:00',
                 'eventSignupEndTime' => '2019-08-24 18:00:00',
                 'eventRunStartTime' => '2019-11-24 09:00:00',
                 'eventRunEndTime' => '2019-11-24 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2019新北場',
                 'eventImage' => 'TPH201912.jpg',
                 'eventTel' => '0800-333-444',
@@ -35,15 +28,9 @@ class EventsTableSeeder extends Seeder
             DB::table('events')->insert([
                 'cityId' => '4',
                 'eventSignupStartTime' => '2019-08-03 09:00:00',
-<<<<<<< HEAD
-                'eventSignupEndTime' => '2019-09-03 09:00:00',
-                'eventRunStartTime' => '2019-11-03 07:00:00',
-                'eventRunEndTime' => '2019-11-03 17:00:00',
-=======
                 'eventSignupEndTime' => '2019-08-03 18:00:00',
                 'eventRunStartTime' => '2019-11-03 07:00:00',
                 'eventRunEndTime' => '2019-11-03 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2019桃園場',
                 'eventImage' => 'TYC201912.jpg',
                 'eventTel' => '0800-333-444',
@@ -53,15 +40,9 @@ class EventsTableSeeder extends Seeder
             DB::table('events')->insert([
                 'cityId' => '7',
                 'eventSignupStartTime' => '2019-09-08 09:00:00',
-<<<<<<< HEAD
-                'eventSignupEndTime' => '2019-10-08 09:00:00',
-                'eventRunStartTime' => '2019-12-08 07:00:00',
-                'eventRunEndTime' => '2019-12-08 17:00:00',
-=======
                 'eventSignupEndTime' => '2019-09-08 18:00:00',
                 'eventRunStartTime' => '2019-12-08 07:00:00',
                 'eventRunEndTime' => '2019-12-08 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2019台中場',
                 'eventImage' => 'TXG201912.jpg',
                 'eventTel' => '0800-555-666',
@@ -70,17 +51,10 @@ class EventsTableSeeder extends Seeder
 
             DB::table('events')->insert([
                 'cityId' => '8',
-<<<<<<< HEAD
-                'eventSignupStartTime' => '2019-12-06 09:00:00',
-                'eventSignupEndTime' => '2020-01-06 09:00:00',
-                'eventRunStartTime' => '2020-02-09 07:00:00',
-                'eventRunEndTime' => '2019-02-09 17:00:00',
-=======
                 'eventSignupStartTime' => '2019-11-09 09:00:00',
                 'eventSignupEndTime' => '2019-11-09 18:00:00',
                 'eventRunStartTime' => '2020-02-09 07:00:00',
                 'eventRunEndTime' => '2020-02-09 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2020彰化場',
                 'eventImage' => 'CWH202002.jpg',
                 'eventTel' => '0800-271-637',
@@ -90,15 +64,9 @@ class EventsTableSeeder extends Seeder
             DB::table('events')->insert([
                 'cityId' => '10',
                 'eventSignupStartTime' => '2019-06-22 09:00:00',
-<<<<<<< HEAD
-                'eventSignupEndTime' => '2019-07-22 09:00:00',
-                'eventRunStartTime' => '2019-09-22 07:00:00',
-                'eventRunEndTime' => '2019-09-22 17:00:00',
-=======
                 'eventSignupEndTime' => '2019-06-22 18:00:00',
                 'eventRunStartTime' => '2019-09-22 07:00:00',
                 'eventRunEndTime' => '2019-09-22 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2019雲林場',
                 'eventImage' => 'YUN201909.jpg',
                 'eventTel' => '0800-777-888',
@@ -107,17 +75,10 @@ class EventsTableSeeder extends Seeder
 
             DB::table('events')->insert([
                 'cityId' => '12',
-<<<<<<< HEAD
-                'eventSignupStartTime' => '2019-07-22 09:00:00',
-                'eventSignupEndTime' => '2019-08-22 09:00:00',
-                'eventRunStartTime' => '2019-10-22 07:00:00',
-                'eventRunEndTime' => '2019-10-22 17:00:00',
-=======
                 'eventSignupStartTime' => '2019-07-20 09:00:00',
                 'eventSignupEndTime' => '2019-07-20 18:00:00',
                 'eventRunStartTime' => '2019-10-20 07:00:00',
                 'eventRunEndTime' => '2019-10-20 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2019台南場',
                 'eventImage' => 'TNN201910.jpg',
                 'eventTel' => '0800-333-444',
@@ -127,15 +88,9 @@ class EventsTableSeeder extends Seeder
             DB::table('events')->insert([
                 'cityId' => '13',
                 'eventSignupStartTime' => '2019-09-29 09:00:00',
-<<<<<<< HEAD
-                'eventSignupEndTime' => '2019-11-30 09:00:00',
-                'eventRunStartTime' => '2019-12-29 07:00:00',
-                'eventRunEndTime' => '2019-12-29 17:00:00',
-=======
                 'eventSignupEndTime' => '2019-09-29 18:00:00',
                 'eventRunStartTime' => '2019-12-29 07:00:00',
                 'eventRunEndTime' => '2019-12-29 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2019高雄場',
                 'eventImage' => 'KHH201912.jpg',
                 'eventTel' => '0800-333-444',
@@ -145,15 +100,9 @@ class EventsTableSeeder extends Seeder
             DB::table('events')->insert([
                 'cityId' => '14',
                 'eventSignupStartTime' => '2019-07-06 09:00:00',
-<<<<<<< HEAD
-                'eventSignupEndTime' => '2019-08-06 09:00:00',
-                'eventRunStartTime' => '2019-10-06 07:00:00',
-                'eventRunEndTime' => '2019-10-06 17:00:00',
-=======
                 'eventSignupEndTime' => '2019-07-06 18:00:00',
                 'eventRunStartTime' => '2019-10-06 07:00:00',
                 'eventRunEndTime' => '2019-10-06 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => '2019屏東場',
                 'eventImage' => 'PCH202001.jpg',
                 'eventTel' => '0800-111-222',
@@ -162,17 +111,10 @@ class EventsTableSeeder extends Seeder
 
             DB::table('events')->insert([
                 'cityId' => '15',
-<<<<<<< HEAD
-                'eventSignupStartTime' => '2019-11-05 09:00:00',
-                'eventSignupEndTime' => '2019-11-22 09:00:00',
-                'eventRunStartTime' => '2020-01-05 07:00:00',
-                'eventRunEndTime' => '2020-01-05 17:00:00',
-=======
                 'eventSignupStartTime' => '2019-10-05 09:00:00',
                 'eventSignupEndTime' => '2019-10-05 18:00:00',
                 'eventRunStartTime' => '2020-01-05 07:00:00',
                 'eventRunEndTime' => '2020-01-05 11:00:00',
->>>>>>> fc964a8dec374ed4f10ec3deb9a3432e5b01ae2a
                 'eventTittle' => ' 2020宜蘭場',
                 'eventImage' => 'ILN202001.jpg',
                 'eventTel' => '0800-111-222',
