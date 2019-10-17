dded65a18aa98e2ff228cd99f50917b2a97d85b2
Mikl <asfx159753@gmail.com>
2019-10-17 17:50:30 +0800

    cover201910171750-mikl

--- resources/views/eventAdmin/create.blade.php
+++ resources/views/eventAdmin/create.blade.php
@@ -1,146 +1,144 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        <script src="https://cdn.ckeditor.com/4.13.0/standard/ckeditor.js"></script>
-
-
-
-        <title>個人會員資料管理</title>
-        <style>
-            .Top {
-                width: auto;
-                height: 200px;
-                position: relative;
-                background-image: url("/img/header.jpg");
-                background-size: cover;
-            }
-            .Left {
-                /* width: 25%; */
-                
-                height: auto;
-                position: relative;
-                background-color: yellow;
-                display: inline-block;
-            }
-            .Right {
-                
-                
-                height: auto;
-                position: absolute;
-                background-color:#F0F8FF;
-                display: inline-block;
-            }
-            #eventBag{
-                padding: 10px;
-            }
-        </style>
-
-
-    </head>
+        <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>首頁</title>
+            
+                <!-- Bootstrap -->
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            
+                <!-- Awesome -->
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+                    integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+            
+                <style>
+                    .Top {
+                        width: auto;
+                        /* height: 200px; */
+                        height: 50px;
+                        position: relative;
+            
+                        /* background-image: url("/img/header.jpg"); */
+                        /* background-size: cover; */
+                    }
+            
+                    .Left {
+            
+                        /* width: 25%; */
+                        width: 200px;
+                        height: auto;
+                        position: relative;
+                        background-color: yellow;
+                        display: inline-block;
+                    }
+            
+                    .Right {
+                        width: 85%;
+                        height: auto;
+                        position: absolute;
+                        background-color: #F0F8FF;
+                        display: inline-block;
+            
+                    }
+            
+                    .content {}
+            
+                </style>
+            </head>
 
 <body>
     <!--------------------------------------------------------------------------------------TOP-------------------------------------------------------------------------------------->
-    <div class="Top">
-
-
-
-    </div>
+    <div class="Top bg-dark">
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a href="/ui"><i class="fas fa-landmark fa-2x"></i>首頁</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <a href="/logout"><i class="fas fa-sign-out-alt fa-2x"></i>登出</a>
+                    </ul>
+                </div>
+            </nav>
+        </div>
 
     <!--------------------------------------------------------------------------------------content-------------------------------------------------------------------------------------->
-    <div class="Left col-2">
-        <div class="accordion" id="accordionExample">
-            <div class="card">
-                <div class="card-header" id="headingOne">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link " type="button" data-toggle="collapse" data-target="#collapseOne"
-                            aria-expanded="true" aria-controls="collapseOne">
-                            會員資料管理
-                        </button>
-                    </h2>
-                </div>
-
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-                    data-parent="#accordionExample">
-                    <div class="card-body">
-                        <a class="dropdown-item" href="/memberAdmin">個人會員管理</a>
-                        <a class="dropdown-item" href="#">團體會員管理</a>
+    <div class="Left">
+            <div class="accordion" id="accordionExample">
+                <div class="card">
+                    <div class="card-header" id="headingOne">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                                aria-expanded="true" aria-controls="collapseOne">
+                                會員及訂單管理
+                            </button>
+                        </h2>
+                    </div>
+    
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                        data-parent="#accordionExample">
+                        <div class="card-body">
+                            <a class="dropdown-item" href="/memberAdmin">會員管理</a>
+                            <a class="dropdown-item" href="/orderGroupAdmin">訂單管理</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card">
-                <div class="card-header" id="headingTwo">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link " type="button" data-toggle="collapse"
-                            data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            賽事管理
-                        </button>
-                    </h2>
-                </div>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                    <div class="card-body">
-                        <a class="dropdown-item" href="/eventAdmin">賽事列表</a>
-                        {{-- <a class="dropdown-item" href="#">賽事編輯</a> --}}
+                <div class="card">
+                    <div class="card-header" id="headingTwo">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                賽事管理
+                            </button>
+                        </h2>
+                    </div>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <a class="dropdown-item" href="/eventAdmin">賽事列表</a>
+                            {{-- <a class="dropdown-item" href="#">賽事編輯</a> --}}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card">
-                <div class="card-header" id="headingThree">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
-                            data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            訂單管理
-                        </button>
-                    </h2>
-                </div>
-                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                    <div class="card-body">
-                        <a class="dropdown-item" href="#">個人訂單管理</a>
-                        <a class="dropdown-item" href="#">團體訂單管理</a>
+                <!-- <div class="card">
+                    <div class="card-header" id="headingThree">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                訂單管理
+                            </button>
+                        </h2>
                     </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header" id="headingThree">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
-                            data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                            賽事相片管理
-                        </button>
-                    </h2>
-                </div>
-                <div id="collapseFour" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                    <div class="card-body">
-                        <a class="dropdown-item" href="#">賽事相片新增</a>
-                        <a class="dropdown-item" href="#">賽事相片列表</a>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <a class="dropdown-item" href="#">個人訂單管理</a>
+                            <a class="dropdown-item" href="#">團體訂單管理</a>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header" id="headingThree">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
-                            data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                            贊助商管理
-                        </button>
-                    </h2>
-                </div>
-                <div id="collapseFive" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                    <div class="card-body">
-                        <a class="dropdown-item" href="#">贊助商新增</a>
-                        <a class="dropdown-item" href="#">贊助商列表</a>
+                </div> -->
+                <div class="card">
+                    <div class="card-header" id="headingFour">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                帳號管理
+                            </button>
+                        </h2>
+                    </div>
+                    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <a class="dropdown-item" href="/accountAdmin"> 帳號列表</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
-    </div>
    
         <div class="Right col-10" id="rightFunction">
                 <form method="post" action="/eventAdmin" class="form-horizontal">
